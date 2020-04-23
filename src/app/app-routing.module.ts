4c1db685053c061beafd495776df98c58ba3d0e4
Robin Huy <robin@techmaster.vn>
2020-04-23 17:51:31 +0700

    config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./pages/login/login.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { ProfileComponent } from "./pages/profile/profile.component";
+import { UsersComponent } from "./pages/users/users.component";
+import { CreateUserComponent } from "./pages/users/create-user/create-user.component";
+import { EditUserComponent } from "./pages/users/edit-user/edit-user.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "home", component: HomeComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "users", component: UsersComponent },
+  { path: "users/create", component: CreateUserComponent },
+  { path: "users/edit", component: EditUserComponent },
+  { path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { UsersComponent } from './pages/users/users.component';
+import { CreateUserComponent } from './pages/users/create-user/create-user.component';
+import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ProfileComponent,
+    UsersComponent,
+    CreateUserComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
